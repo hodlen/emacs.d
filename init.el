6f09f74aa3644f499809001df1a712ce5c23f071
Holden <34213478+hodlen@users.noreply.github.com>
2023-06-19 11:04:09 +0800

    speedup terminal startup

--- init.el
+++ init.el
@@ -21,7 +21,6 @@
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
-
 ;; Adjust garbage collection thresholds during startup, and thereafter
 
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
@@ -30,150 +29,152 @@
   (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
-
 ;; Bootstrap config
+(require 'bootstrap-local) ;; Bootstrap custom settings
 
+;; Load rich features only in GUI Emacs
+(when window-system
+  (progn
+    (setq custom-file (locate-user-emacs-file "custom.el"))
+    (require 'init-utils)
+    (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+    ;; Calls (package-initialize)
+    (require 'init-elpa) ;; Machinery for installing required packages
+    (require 'init-exec-path) ;; Set up $PATH
 
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(require 'init-utils)
-(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
-;; Calls (package-initialize)
-(require 'init-elpa)      ;; Machinery for installing required packages
-(require 'init-exec-path) ;; Set up $PATH
+    ;; Allow users to provide an optional "init-preload-local.el"
+    (require 'init-preload-local nil t)
 
-
-;; Allow users to provide an optional "init-preload-local.el"
-(require 'init-preload-local nil t)
+    ;; Load configs for specific features and modes
+    (require-package 'diminish)
+    (maybe-require-package 'scratch)
+    (require-package 'command-log-mode)
 
-;; Load configs for specific features and modes
-(require-package 'diminish)
-(maybe-require-package 'scratch)
-(require-package 'command-log-mode)
+    (require 'init-frame-hooks)
+    (require 'init-xterm)
+    (require 'init-themes)
+    (require 'init-osx-keys)
+    (require 'init-gui-frames)
+    (require 'init-dired)
+    (require 'init-isearch)
+    (require 'init-grep)
+    (require 'init-uniquify)
+    (require 'init-ibuffer)
+    (require 'init-flymake)
+    (require 'init-eglot)
 
-(require 'init-frame-hooks)
-(require 'init-xterm)
-(require 'init-themes)
-(require 'init-osx-keys)
-(require 'init-gui-frames)
-(require 'init-dired)
-(require 'init-isearch)
-(require 'init-grep)
-(require 'init-uniquify)
-(require 'init-ibuffer)
-(require 'init-flymake)
-(require 'init-eglot)
+    (require 'init-recentf)
+    (require 'init-minibuffer)
+    (require 'init-hippie-expand)
+    (require 'init-corfu)
+    (require 'init-windows)
+    ;; (require 'init-sessions)
+    (require 'init-mmm)
 
-(require 'init-recentf)
-(require 'init-minibuffer)
-(require 'init-hippie-expand)
-(require 'init-corfu)
-(require 'init-windows)
-;; (require 'init-sessions)
-(require 'init-mmm)
+    (require 'init-editing-utils)
+    (require 'init-whitespace)
 
-(require 'init-editing-utils)
-(require 'init-whitespace)
+    (require 'init-vc)
+    (require 'init-darcs)
+    (require 'init-git)
+    (require 'init-github)
 
-(require 'init-vc)
-(require 'init-darcs)
-(require 'init-git)
-(require 'init-github)
+    (require 'init-projectile)
 
-(require 'init-projectile)
+    (require 'init-compile)
+    (require 'init-crontab)
+    (require 'init-textile)
+    (require 'init-markdown)
+    (require 'init-csv)
+    (require 'init-erlang)
+    (require 'init-javascript)
+    (require 'init-php)
+    (require 'init-org)
+    (require 'init-nxml)
+    (require 'init-html)
+    (require 'init-css)
+    (require 'init-haml)
+    (require 'init-http)
+    (require 'init-python)
+    (require 'init-haskell)
+    (require 'init-elm)
+    (require 'init-purescript)
+    (require 'init-ruby)
+    (require 'init-rails)
+    (require 'init-sql)
+    (require 'init-ocaml)
+    (require 'init-j)
+    (require 'init-nim)
+    (require 'init-rust)
+    (require 'init-toml)
+    (require 'init-yaml)
+    (require 'init-docker)
+    (require 'init-terraform)
+    (require 'init-nix)
+    (maybe-require-package 'nginx-mode)
 
-(require 'init-compile)
-(require 'init-crontab)
-(require 'init-textile)
-(require 'init-markdown)
-(require 'init-csv)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-php)
-(require 'init-org)
-(require 'init-nxml)
-(require 'init-html)
-(require 'init-css)
-(require 'init-haml)
-(require 'init-http)
-(require 'init-python)
-(require 'init-haskell)
-(require 'init-elm)
-(require 'init-purescript)
-(require 'init-ruby)
-(require 'init-rails)
-(require 'init-sql)
-(require 'init-ocaml)
-(require 'init-j)
-(require 'init-nim)
-(require 'init-rust)
-(require 'init-toml)
-(require 'init-yaml)
-(require 'init-docker)
-(require 'init-terraform)
-(require 'init-nix)
-(maybe-require-package 'nginx-mode)
+    (require 'init-paredit)
+    (require 'init-lisp)
+    (require 'init-slime)
+    (require 'init-clojure)
+    (require 'init-clojure-cider)
+    (require 'init-common-lisp)
 
-(require 'init-paredit)
-(require 'init-lisp)
-(require 'init-slime)
-(require 'init-clojure)
-(require 'init-clojure-cider)
-(require 'init-common-lisp)
+    (when *spell-check-support-enabled*
+      (require 'init-spelling))
 
-(when *spell-check-support-enabled*
-  (require 'init-spelling))
+    (require 'init-misc)
 
-(require 'init-misc)
+    (require 'init-folding)
+    (require 'init-dash)
 
-(require 'init-folding)
-(require 'init-dash)
+    ;;(require 'init-twitter)
+    ;; (require 'init-mu)
+    (require 'init-ledger)
+    ;; Extra packages which don't require any configuration
 
-;;(require 'init-twitter)
-;; (require 'init-mu)
-(require 'init-ledger)
-;; Extra packages which don't require any configuration
+    (require-package 'sudo-edit)
+    (require-package 'gnuplot)
+    (require-package 'lua-mode)
+    (require-package 'htmlize)
+    (when *is-a-mac*
+      (require-package 'osx-location))
+    (maybe-require-package 'dotenv-mode)
+    (maybe-require-package 'shfmt)
 
-(require-package 'sudo-edit)
-(require-package 'gnuplot)
-(require-package 'lua-mode)
-(require-package 'htmlize)
-(when *is-a-mac*
-  (require-package 'osx-location))
-(maybe-require-package 'dotenv-mode)
-(maybe-require-package 'shfmt)
+    (when (maybe-require-package 'uptimes)
+      (setq-default uptimes-keep-count 200)
+      (add-hook 'after-init-hook (lambda () (require 'uptimes))))
 
-(when (maybe-require-package 'uptimes)
-  (setq-default uptimes-keep-count 200)
-  (add-hook 'after-init-hook (lambda () (require 'uptimes))))
+    (when (fboundp 'global-eldoc-mode)
+      (add-hook 'after-init-hook 'global-eldoc-mode))
 
-(when (fboundp 'global-eldoc-mode)
-  (add-hook 'after-init-hook 'global-eldoc-mode))
+    (require 'init-direnv)
 
-(require 'init-direnv)
+    ;; Allow access from emacsclient
+    (add-hook 'after-init-hook
+              (lambda ()
+                (require 'server)
+                (unless (server-running-p)
+                  (server-start))))
 
-
+    ;; Variables configured via the interactive 'customize' interface
+    (when (file-exists-p custom-file)
+      (load custom-file))
 
-;; Allow access from emacsclient
-(add-hook 'after-init-hook
-          (lambda ()
-            (require 'server)
-            (unless (server-running-p)
-              (server-start))))
+    ;; Locales (setting them earlier in this file doesn't work in X)
+    (require 'init-locales)
 
-;; Variables configured via the interactive 'customize' interface
-(when (file-exists-p custom-file)
-  (load custom-file))
+    ;; Allow users to provide an optional "init-local" containing personal settings
+    (require 'init-local nil t)
 
-;; Locales (setting them earlier in this file doesn't work in X)
-(require 'init-locales)
+    (provide 'init)
 
-;; Allow users to provide an optional "init-local" containing personal settings
-(require 'init-local nil t)
+    ;; Local Variables:
+    ;; coding: utf-8
+    ;; no-byte-compile: t
+    ;; End:
+    )
+  )
 
-(provide 'init)
-
-;; Local Variables:
-;; coding: utf-8
-;; no-byte-compile: t
-;; End:
-;;; init.el ends here
+;;; init.el ends heer
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,30 +1,9 @@
 ;;; --- My customization for Emacs on Purcell's defaults
 
 (require 'auto-dark)
-(auto-dark-mode t)
+(ignore-errors (auto-dark-mode t))
 
 ;; auto-enable Company Coq
 (add-hook 'coq-mode-hook #'company-coq-mode)
-
-(setq-default tab-width 4)
-
-(defun set-tab (width)
-  "Set the tab width in the current buffer to the specified value."
-  (interactive "nTab width: ")
-  (setq-local tab-width width))
-
-(setq-default indent-tabs-mode nil)
-
-;; the tabs plugin
-;; (require 'centaur-tabs)
-;; (centaur-tabs-headline-match)
-;; (centaur-tabs-mode t)
-;; (global-set-key (kbd "C-<prior>")  'centaur-tabs-backward)
-;; (global-set-key (kbd "C-<next>") 'centaur-tabs-forward)
-
-;; open recent files
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 (provide 'init-local)
