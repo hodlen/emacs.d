7336a25320b7d3c4cb795cdf6150145507878404
Holden <hodlenx@gmail.com>
2024-03-28 14:27:11 +0800

    fix auto-dark hung

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,6 +1,6 @@
 ;;; --- My customization for Emacs on Purcell's defaults
-(require-package 'auto-dark)
-(ignore-errors (auto-dark-mode t))
+(use-package auto-dark
+  :config (auto-dark-mode t))
 
 ;; auto-enable Company Coq
 (add-hook 'coq-mode-hook #'company-coq-mode)
