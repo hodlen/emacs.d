3ad5ea895c84908d8a04cc93bcc2ed0e41881834
Holden <hodlenx@gmail.com>
2025-09-01 21:22:02 +0800

    move custom.el to temp(.cache) dir

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -17,4 +17,17 @@
 (when (maybe-require-package 'clipetty)
   (add-hook 'after-init-hook 'global-clipetty-mode))
 
+;; Hand-written customizations (synced from customize-group)
+(setq auto-dark-allow-osascript t)
+(setq auto-dark-themes '((sanityinc-tomorrow-bright) (sanityinc-tomorrow-day)))
+(setq backup-directory-alist '(("\".*\"" . "~/.cache/emacs/backup")))
+(setq custom-enabled-themes '(sanityinc-tomorrow-bright))
+(setq grep-command "grep --color=auto -nH --null -r -e")
+(setq grep-highlight-matches t)
+(setq tab-width 4)
+
+;; Set custom-file to cache location outside .emacs.d
+(setq custom-file "~/.cache/emacs/custom.el")
+(load custom-file 'noerror)
+
 (provide 'init-local)
